0efb46dc0d3bbdc7f3fe84624757103e427f4bb4
Nooralwachi <noor_alwachi@yahoo.com>
2020-12-05 13:09:35 -0800

    Add files via upload

---
+++ Report_Repair.py
@@ -0,0 +1,22 @@
+def report_repair(file):
+	s= set()
+	numbers=[]
+	f = open(file, "r")
+	
+	numbers.append(f.read())
+	list_of_numbers=numbers[0].split("\n")
+	#print(list_of_numbers)
+	for i in list_of_numbers:
+		if (i !='  '):
+			if (2020- int(i)) not in s:
+				s.add(int(i))
+			else:
+				result = int(i) * (2020- int(i))
+	print(s)
+	print(result)
+	
+
+
+
+
+report_repair("input1.txt")
